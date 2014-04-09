bd27c26c81807da44f106f7ce0186a9c7fa7aa3a
shansen <sebastian@bragehansen.com>
2014-04-09 08:23:42 +0200

    Changed the app to mimic the invitation

---
+++ javascripts/base.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	var _now = new Date().getTime(),
+		_specialDay = new Date( "2014-7-5 00:00" ).getTime(),
+		$schedualItems = $(".agenda").children().not(".header"),
+		_noActiveItem = true;
+		
+		console.log( _now );
+	
+	$schedualItems.each( function(){
+	
+		var $item = $(this),
+			$time = $item.find("time"),
+			_activetyTime = new Date( $time.attr( "datetime" ) ).getTime();
+
+		console.log( _activetyTime );
+		
+		if(  _now >= _specialDay ){
+			if( _activetyTime >= _now ){
+				if( _noActiveItem ){
+					$item.prev().attr( "class", "is-happening" );
+					_noActiveItem = false;
+				}
+			}
+			else {
+				$item.addClass( "has-happened" );
+			}
+		}
+	});
+});
