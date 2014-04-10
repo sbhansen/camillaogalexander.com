2f59774a07e0fc7871feed904be230c3812396b4
Sebastian Brage Hansen <sebastian@bragehansen.com>
2014-04-10 23:38:36 +0200

    Fancy card sliders

--- javascripts/base.js
+++ javascripts/base.js
@@ -1,69 +1,32 @@
 $(document).ready(function(){
 	updateScheduel();
-	window.loop = setInterval( updateScheduel, 60000 );
+	window.loop = setInterval( updateScheduel, 60000 );		$cards = $("#title, .framed");	$clones = $cards.clone();	$cards.addClass("card");	$clones.addClass("clone");	$("body").prepend( $clones );	
 	$( window ).on("scroll", function(){
-		
-		if( window.location.search.indexOf("unicornsAreNotHorses") <= 0 )
-			return false;
+						var winHeight = $( window ).height(),			winTop = $( window ).scrollTop(),			winBottom = winTop + winHeight;				//console.log( winBottom );
+		// Bruk en clone av det orginale objectet som fixed og sett visibility hidden på orginalen.	
+		$cards.each( function( i ){
 			
-		// Bruk en clone av det orginale objectet som fixed og sett visibility hidden på orginalen.
-			
-		$frames = $(".framed");
-	
-		$frames.each( function( i ){
-			
-			var offset = $( this ).offset(),
-				elmHeight = $( this ).outerHeight(),
+			var $current = $( this ),				$clone = $( $clones[ i ] ),				offset = $current.offset(),
+				elmHeight = $current.height(),
 				elmTop = offset.top,
-				elmBottom = elmTop + elmHeight,
-				winHeight = $( window ).height(),
-				winTop = $( window ).scrollTop(),
-				winBottom = winTop + winHeight;
-							
-				if( winBottom > elmBottom ){
-					$( this ).addClass( "pinned" );
-					$( this ).next().css("margin-top", elmHeight );
+				elmBottom = elmTop + elmHeight;			
+				if( winBottom >= elmBottom && elmTop - winTop <= 0 ){					if( elmHeight >= winHeight ){						$current.addClass("bottom");												$clone.addClass("bottom");
+					}
+					$current.addClass("pinned");											$clone.addClass("pinned");
 				}
 				else {
-					$( this ).removeClass( "pinned" );
-					$( this ).next().css("margin-top", 0 );
+					$current.removeClass("pinned bottom");
+					$clone.removeClass("pinned bottom");
 				}
 		});
-		if( $( this ).scrollTop() > 0 ){
-			$("body").addClass("scroll");
-		}
-		else {
-			$("body").removeClass("scroll");
-		}
-	});
-});
+	});});
 
 function updateScheduel(){
-	var _now = new Date().getTime(),
-		_specialDay = new Date( "2014-7-5 00:00" ).getTime(),
-		_afterSpecialDay = new Date( "2014-7-6 10:00" ).getTime(),
-		$schedualItems = $(".agenda").children().not(".header"),
-		_noActiveItem = true;
-	
+	var _now = new Date().getTime(),		_specialDay = new Date( "2014-7-5 00:00" ).getTime(),		_afterSpecialDay = new Date( "2014-7-6 10:00" ).getTime(),		$schedualItems = $(".agenda").children().not(".header"),		_noActiveItem = true;	
 	if(  _now >= _specialDay && _now < _afterSpecialDay ){
-		
-		$schedualItems.each( function( i ){
-	
-			var $item = $(this),
-				$time = $item.find("time"),
-				_activetyTime = new Date( $time.attr( "datetime" ) ).getTime();	
-		
-			if( _activetyTime >= _now ){
-				if( _noActiveItem && i ){
-					var y = i - 1;
-					$( $schedualItems[ y ] ).attr( "class", "is-happening" );
-					_noActiveItem = false;
-				}
-			}
-			else {
-				$item.addClass( "has-happened" );
-			}
+		$schedualItems.each( function( i ){			var $item = $(this),				$time = $item.find("time"),				_activetyTime = new Date( $time.attr( "datetime" ) ).getTime();	
+			if( _activetyTime >= _now ){				if( _noActiveItem && i ){
+					var y = i - 1;					$( $schedualItems[ y ] ).attr( "class", "is-happening" );					_noActiveItem = false;				}			}			else {				$item.addClass( "has-happened" );			}
 		});
-		console.log("doing");		
 	}
 }
