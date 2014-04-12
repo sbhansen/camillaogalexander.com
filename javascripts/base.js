787caf7f133b24626bc194038c3476b095d9fb23
Sebastian Brage Hansen <sebastian@bragehansen.com>
2014-04-12 11:07:37 +0200

    Added fixed heading for mobile. Did some text changes.

--- javascripts/base.js
+++ javascripts/base.js
@@ -1,29 +1,12 @@
-$(document).ready(function(){
-	var _now = new Date().getTime(),
-		_specialDay = new Date( "2014-7-5 00:00" ).getTime(),
-		$schedualItems = $(".agenda").children().not(".header"),
-		_noActiveItem = true;
-		
-		console.log( _now );
-	
-	$schedualItems.each( function(){
-	
-		var $item = $(this),
-			$time = $item.find("time"),
-			_activetyTime = new Date( $time.attr( "datetime" ) ).getTime();
-
-		console.log( _activetyTime );
-		
-		if(  _now >= _specialDay ){
-			if( _activetyTime >= _now ){
-				if( _noActiveItem ){
-					$item.prev().attr( "class", "is-happening" );
-					_noActiveItem = false;
-				}
-			}
-			else {
-				$item.addClass( "has-happened" );
-			}
-		}
-	});
-});
+$(document).ready(function(){	updateScheduel();	spaceTitleFromContent();
+	window.loop = setInterval( updateScheduel, 60000 );	$( window ).on("resize", spaceTitleFromContent );});
+
+function updateScheduel(){
+	var _now = new Date().getTime(),		_specialDay = new Date( "2014-7-5 00:00" ).getTime(),		_afterSpecialDay = new Date( "2014-7-6 10:00" ).getTime(),		$schedualItems = $(".agenda").children().not(".header"),		_noActiveItem = true;	
+	if(  _now >= _specialDay && _now < _afterSpecialDay ){
+		$schedualItems.each( function( i ){			var $item = $(this),				$time = $item.find("time"),				_activetyTime = new Date( $time.attr( "datetime" ) ).getTime();	
+			if( _activetyTime >= _now ){				if( _noActiveItem && i ){
+					var y = i - 1;					$( $schedualItems[ y ] ).attr( "class", "is-happening" );					_noActiveItem = false;				}			}			else {				$item.addClass( "has-happened" );			}
+		});
+	}
+}function spaceTitleFromContent(){	var $title = $("#title"),		$content = $("#content"),		_marginTop = $content.data("margin-top") ? $content.data("margin-top") : $content.css("margin-top");		if( $title.width() <= 500 ){		$content.css({ "margin-top": $title.height() });		$content.data("margin-top", _marginTop );	}	else {		$content.css({ "margin-top": _marginTop });	}	}
