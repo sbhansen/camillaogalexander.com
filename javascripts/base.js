f484056e24ef517283606f893ba1a29369afd7bb
Sebastian Brage Hansen <sebastian@bragehansen.com>
2014-04-12 10:28:57 +0200

    Added Iscroller to probe events properly on touch phones.

--- javascripts/base.js
+++ javascripts/base.js
@@ -1,26 +1,7 @@
 $(document).ready(function(){
 	updateScheduel();
-	window.loop = setInterval( updateScheduel, 60000 );		$cards = $("#title, .framed");	$clones = $cards.clone();	$cards.addClass("card");	$clones.addClass("clone");	$("body").prepend( $clones );	
-	$( window ).on("scroll", function(){
-						var winHeight = $( window ).height(),			winTop = $( window ).scrollTop(),			winBottom = winTop + winHeight;				//console.log( winBottom );
-		// Bruk en clone av det orginale objectet som fixed og sett visibility hidden på orginalen.	
-		$cards.each( function( i ){
-			
-			var $current = $( this ),				$clone = $( $clones[ i ] ),				offset = $current.offset(),
-				elmHeight = $current.height(),
-				elmTop = offset.top,
-				elmBottom = elmTop + elmHeight;			
-				if( winBottom >= elmBottom && elmTop - winTop <= 0 ){					if( elmHeight >= winHeight ){						$current.addClass("bottom");												$clone.addClass("bottom");
-					}
-					$current.addClass("pinned");											$clone.addClass("pinned");
-				}
-				else {
-					$current.removeClass("pinned bottom");
-					$clone.removeClass("pinned bottom");
-				}
-		});
-	});});
-
+	window.loop = setInterval( updateScheduel, 60000 );		myScroll = new IScroll('#scrollWrapper', {		probeType: 3,		mouseWheel: true,	});		myScroll.on('scroll', snapulate );	myScroll.on('scrollEnd', snapulate );		document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);		$cards = $(" #content > div ");	$clones = $cards.clone();	$cards.addClass("card");	$clones.addClass("clone");	$("#clones").append( $clones );});
+function snapulate(){	var winHeight = $( window ).height(),		winTop = - this.y,		winBottom = winTop + winHeight,		cardsHeight = - $cards.last().innerHeight();			$cards.each( function(){ cardsHeight += $(this).innerHeight() });				myScroll.maxScrollY = -cardsHeight;		$cards.each( function( i ){				var $current = $( this ),			$clone = $( $clones[ i ] ),			offset = $current.offset(),			elmHeight = $current.height(),			elmTop = offset.top,			elmBottom = elmTop + elmHeight;				if( elmHeight >= winHeight ){			if( elmBottom <= winHeight ){				$clone.addClass("pinned bottom");				$current.addClass("pinned bottom");													}			else {				$current.removeClass("pinned bottom");				$clone.removeClass("pinned bottom");			}		}		else {			if( elmTop <= 0 ){				$clone.addClass("pinned");				$current.addClass("pinned");			}			else {				$current.removeClass("pinned bottom");				$clone.removeClass("pinned bottom");			}		}	});}
 function updateScheduel(){
 	var _now = new Date().getTime(),		_specialDay = new Date( "2014-7-5 00:00" ).getTime(),		_afterSpecialDay = new Date( "2014-7-6 10:00" ).getTime(),		$schedualItems = $(".agenda").children().not(".header"),		_noActiveItem = true;	
 	if(  _now >= _specialDay && _now < _afterSpecialDay ){
